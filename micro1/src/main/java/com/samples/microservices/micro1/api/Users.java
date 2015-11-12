7177589e6f0162c451f7e551624ce91e4fe70d1f
Assaf Kamil <assafkamil@gmail.com>
2015-11-12 15:52:18 +0200

    Feign for all the others

--- micro1/src/main/java/com/samples/microservices/micro1/api/Users.java
+++ micro1/src/main/java/com/samples/microservices/micro1/api/Users.java
@@ -16,10 +16,10 @@ public interface Users {
     @RequestMapping(value="/users/{id}", method = RequestMethod.GET, consumes = "application/json")
     public UserResponse getById(@PathVariable("id") String id);
 
-    @RequestMapping(value="/users", method = RequestMethod.GET)
+    @RequestMapping(value="/users", method = RequestMethod.GET, consumes = "application/json")
     public List<UserResponse> getAll(@RequestParam(value = "start", defaultValue = "0") int start,
                                      @RequestParam(value = "limit", defaultValue = "-1") int limit);
 
-    @RequestMapping(value="/users2", method = RequestMethod.GET)
+    @RequestMapping(value="/users2", method = RequestMethod.GET, consumes = "application/json")
     public UserResponses getAll2(@RequestParam(value = "cursor", defaultValue = "") String cursor);
 }
