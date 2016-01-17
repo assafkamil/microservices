6ab7ca5000e00564c3e391de6fe5d5b21049f1c4
Assaf Kamil <assafkamil@gmail.com>
2016-01-17 23:15:31 +0200

    Potential fix for war in tomcat8

--- micro1/src/main/java/com/samples/microservices/micro1/Application.java
+++ micro1/src/main/java/com/samples/microservices/micro1/Application.java
@@ -2,12 +2,19 @@ package com.samples.microservices.micro1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.samples")
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
